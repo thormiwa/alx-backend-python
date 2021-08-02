78a679b23dbcfbf2bde4032e247860b6ace54917
thormiwa <thormiwa04@gmail.com>
2021-08-02 14:53:29 +0000

    make multi

---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+'''This module contains an make_multiplier function'''
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    '''takes a float and return a function'''
+    def my_func(multiply:float) -> float:
+        '''multiplies a float by a function'''
+        return multiplier * multiply
+    return my_func
