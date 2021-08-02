210f26906bc201fbca50324223714e8ce160361a
thormiwa <70862950+thormiwa@users.noreply.github.com>
2021-08-02 15:56:40 +0100

    Update 8-make_multiplier.py

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -5,7 +5,7 @@ from typing import Callable
 
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
     '''takes a float and return a function'''
-    def my_func(multiply:float) -> float:
+    def my_func(multiply: float) -> float:
         '''multiplies a float by a function'''
         return multiplier * multiply
     return my_func
