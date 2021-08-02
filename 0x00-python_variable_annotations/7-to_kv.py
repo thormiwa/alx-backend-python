8539bf29fd922354d76d029033bddd6b52cd22f8
thormiwa <70862950+thormiwa@users.noreply.github.com>
2021-08-02 15:52:27 +0100

    Update 7-to_kv.py

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -3,6 +3,6 @@
 from typing import List, Union, Tuple
 
 
-def to_kv(k:str, v:Union[int, float]) -> Tuple[str, float]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     '''returns a string and multiplies the float by 2'''
     return(k, v ** 2)
