5b676152ee478d64cd280ea0a70d494513daefb3
thormiwa <70862950+thormiwa@users.noreply.github.com>
2021-08-02 15:37:06 +0100

    Update 1-concat.py

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -2,6 +2,6 @@
 '''This module contains a concat function'''
 
 
-def concat(str1:str, str2:str) -> str:
+def concat(str1: str, str2: str) -> str:
     '''Concantenates two strings and return a string'''
     return str1 + str2
