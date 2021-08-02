6d470ae48a827fa987cf9b7b779ad6ab296689b1
thormiwa <70862950+thormiwa@users.noreply.github.com>
2021-08-02 14:36:37 +0100

    Update 0-add.py

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 '''The addition module'''
 
+
 def add(a:float, b:float) -> float:
     '''To add according to datatype and return their sum'''
-    return (a + b)
+    return a + b
