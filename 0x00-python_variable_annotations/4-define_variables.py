1121ae26797a6c015cb7c307ab81dc3f5f4b96f1
thormiwa <70862950+thormiwa@users.noreply.github.com>
2021-08-02 15:39:23 +0100

    Update 4-define_variables.py

--- 0x00-python_variable_annotations/4-define_variables.py
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -2,7 +2,7 @@
 '''This module contains a variable annotation definition'''
 
 
-a:int = 1
-pi:float = 3.14
-i_understand_annotations:bool = True
-school:str = 'Holberton'
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = 'Holberton'
